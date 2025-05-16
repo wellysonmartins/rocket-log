0991e7963f77f9f8ba8d4c508b22d91f559e0ed4
Wellyson Martins <wellysonmartins@gmail.com>
2025-05-16 10:48:06 -0300

    add zod schema

--- src/controllers/users-controlle.ts
+++ src/controllers/users-controlle.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class UsersController {
   create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(1),
+      email: z.string().email(),
+      password: z.string().trim().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
     return response.json({ message: "ok" });
   }
 }
